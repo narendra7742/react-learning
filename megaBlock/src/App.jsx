43dacbd5d34b6d07aa5a1ae85d31089f0edf1c6f
narendra7742 <nkpotter774257@gmail.com>
2025-07-16 23:08:11 +0530

    New mega project

---
+++ megaBlock/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  console.log("Appwrite URL:", import.meta.env.VITE_APPWRITE_URL);
+  return (
+    <>
+      <h1>A Blog with Appwrite</h1>
+    </>
+  );
+}
+
+export default App;
