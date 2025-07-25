14b83c50796d788f2c697e1475eab4d88bf560b1
narendra7742 <nkpotter774257@gmail.com>
2025-07-25 23:02:20 +0530

    Final touch and mega blog is completed

--- megaBlock/src/components/index.js
+++ megaBlock/src/components/index.js
@@ -6,8 +6,11 @@ import LogoutBtn from './Header/LogoutBtn';
 import RTE from './RTE';
 import Signup from "./Signup"
 import Login from './Login';
-import Post from './Post';
 import PostForm from './post-form/PostForm';
 import PostCard from './PostCard'
+import AuthLayout from './AuthLayout';
+import Button from './Button';
+import Input from './Input';
+import Select from './Select';  
 
-export { Header, Footer, Container, Logo, LogoutBtn, RTE, Signup, Login, Post, PostForm, PostCard};
+export { Header, Footer, Container, Logo, LogoutBtn, RTE, Signup, Login, PostForm, PostCard, AuthLayout, Button, Input, Select };
